79439d5e02271deff33feb582bf1bb81c88329cf
Nathan Nichols <n545n545@Nathans-MacBook-Pro.local>
2020-02-08 14:17:24 -0600

    updating

--- embedded/app.js
+++ embedded/app.js
@@ -3,23 +3,21 @@ const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
 const app = express()
 const port = 3000
-const sport = new SerialPort('/dev/ttyACM0', {baudRate:9600, parser: serialport.parsers.readline('\n')})
+const linuxport = '/dev/ttyACM0';
+const sport = new SerialPort('/dev/tty.usbmodem14301', {baudRate:9600})
 
 app.get('/', (req, res) => res.send("hello"))
 
 app.get('/off', async (req, res) => {
 	let str = "";
-	sport.write('OFF');
-	await parser.on('data', (line) => res.send(line))
-	
-	res.send(str)
+	sport.write('OFF');	
+	res.send("Turned off")
 })
 
 app.get('/on', (req,res) => {
 	let str = "";
 	sport.write('ON');
- 	parser.on('data', (line) => str = line)
-	setTimeout(res.send(str),1000);
+	res.send("Turned on")
 })
 
-app.listen(port, () => console.log('Example app'))
+app.listen(port, () => console.log('Endpoint to kill power'))
