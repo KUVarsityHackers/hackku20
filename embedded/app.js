96b221934ddfcb774866883b21cd93dbcb3ae343
Nathan Nichols <natenichols@cox.net>
2020-02-08 21:30:48 +0000

    update

--- embedded/app.js
+++ embedded/app.js
@@ -1,25 +1,23 @@
 const express = require('express')
 const SerialPort = require('serialport')
-const Readline = require('@serialport/parser-readline')
 const app = express()
 const port = 3000
-const sport = new SerialPort('/dev/ttyACM0', {baudRate:9600, parser: serialport.parsers.readline('\n')})
+const sport = new SerialPort('/dev/ttyACM0', {baudRate:9600})
+
+var awslot = require('aws-iot-device-skd');
+
+var device = awsIot.device({keyPath: '.certs/})
 
 app.get('/', (req, res) => res.send("hello"))
 
 app.get('/off', async (req, res) => {
-	let str = "";
-	sport.write('OFF');
-	await parser.on('data', (line) => res.send(line))
-	
-	res.send(str)
+	sport.write('OFF')
+	res.send("Turned off");
 })
 
 app.get('/on', (req,res) => {
-	let str = "";
 	sport.write('ON');
- 	parser.on('data', (line) => str = line)
-	setTimeout(res.send(str),1000);
+ 	res.send("Turned on");
 })
 
-app.listen(port, () => console.log('Example app'))
+app.listen(port, () => console.log('app for killing power'))
